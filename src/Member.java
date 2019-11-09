246d8e45fa800e5cc97c7259705bba1a38d92465
OSV2130648 <57408616+OSV2130648@users.noreply.github.com>
2019-11-09 12:43:27 -0700

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,19 +1,38 @@
-public abstract class Member
+import java.io.Serializable;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public abstract class Member implements Serializable
 
 {
-    private String name;
+    public String name;
 
-    private String year;
-
-    private String topFiveMatches;
-
-    public Member (String name, String year)
+    public int year;
+    
+	Scanner kb = new Scanner(System.in);
+    
+	LinkedList<Interest> interestList = new LinkedList<Interest>();
+	
+    public Member (String name, int year) 
 
     {
+
     	this.name = name;
     	this.year = year;
 
-
     }
 
+   public void changeName(String name) {
+	   this.name = name;
+   }
+   
+   public void changeYear(int year) {
+	   this.year = year;
+   }
+   
+//   public void addInterest(String topic, int topicInterest)
+//  {
+//	   
+//  }
+   
 }
