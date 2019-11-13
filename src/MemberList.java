c8e844b39b87519bd108724284a87cd8a12bc665
Juan <51283162+Juanboy-bebop@users.noreply.github.com>
2019-11-13 15:28:57 -0700

    Update MemberList.java
    Added the following methods
    1. save()
    2. load()
    3. remove()
    4. existingMember()

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,32 +1,65 @@
 import java.util.LinkedList;	
 import java.util.Iterator;
-import java.util.Scanner;
-import java.io.Serializable;
-public class MemberList implements Iterator<String>, Serializable
+import java.util.*;
+import java.io.*;
+public class MemberList implements Iterable<Member>, Serializable
 
+{	//Create List for members
+	//add save and load method for memberList
+	LinkedList<Member> memberList = new LinkedList<Member>();
+	
+public void addMember(String name, int year) 
 {
-	LinkedList<Member> MemberList = new LinkedList<Member>();
-	
-
-	public boolean hasNext() 
-	{
+	//Create member object with parameters for name and year
+	//If Member name is unique then add, if not then change name
+	Member M = new Member(name, year);
 		
-		return false;
-	}
-
-	@Override
-	public String next() 
-	{
-		return null;
-	}
-	
-	
-	public void addMember(String name, int year) 
-	{
-	
-		Member M = new Member("", 5);
+	memberList.add(M);
+		
 		
 	}
-
+	// Instantiate iterator and get its methods
+public Iterator<Member> iterator() 	
+	{
+		return memberList.iterator();
+	}
+	//add method for comparing unique name using for each 
+public Member existingMember(String name)
+{
+	Member found = null;
+	
+	for(Member m: memberList)
+		{
+			if(name.equals(m.getName()))
+			{
+				found = m;
+			}
+		}
+		
+		return found;
+	}
+public void removeMember()
+{
+	memberList.remove();
+}
+	//save file
+public void save(String fileName) throws IOException
+{
+	FileOutputStream fos = new FileOutputStream(fileName); 
+	ObjectOutputStream oos = new ObjectOutputStream(fos); 
+	oos.writeObject(this); 
+	oos.flush(); 
+	oos.close(); 
+}
+	//load members
+public static MemberList load(String fileName) throws IOException, ClassNotFoundException
+{
+	FileInputStream fis = new FileInputStream(fileName);
+	ObjectInputStream ois = new ObjectInputStream(fis);
+	MemberList ml = (MemberList) ois.readObject();
+	ois.close();
+		
+	return ml;
 }
 
+}
