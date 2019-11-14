492410e0130c24bc3a338beaa4fa21510246de17
OSV2130648 <57408616+OSV2130648@users.noreply.github.com>
2019-11-14 07:36:28 -0700

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -9,9 +9,13 @@ public class Member implements Serializable
 
     public int year;
     
+    public int sum;
+    
 	Scanner kb = new Scanner(System.in);
     
 	LinkedList<Interest> interestList = new LinkedList<Interest>();
+	LinkedList<Member> memberList = new LinkedList<Member>();
+	LinkedList<Member> topFive = new LinkedList<Member>();
 	
     public Member (String name, int year) 
 
@@ -32,6 +36,41 @@ public class Member implements Serializable
    
    public void addInterest(String topic, int topicInterest)
   {
-	//   interestList.add();
+	for(Interest in : interestList)
+	{
+		if(topic.equalsIgnoreCase(in.getTopic()))
+		{
+			in.changeTopicIntrest(topicInterest);
+		}
+	}
    }
+   
+   public String getName()
+   {
+	   return name;
+   }
+   
+   
+   public double calculateCompatability(Member m2)
+   {
+	   for( Interest i : this.interestList) 
+	   {
+		 for (Interest i2 : m2.interestList)
+		 {
+			 if(i.getTopic().equalsIgnoreCase(i2.getTopic())) 
+			 {
+				 sum = sum + (i.getTopicInterest() * i2.getTopicInterest());
+			 }
+			 else
+			 {
+				sum = sum + (i2.getTopicInterest() / 2); 
+			 }
+		 }
+	   }
+	  
+	   
+   }
+   
+   
+   
 }
