7b9bf78de66a46ba23c06e5f5ad512aa4d02cd09
OSV2130648 <57408616+OSV2130648@users.noreply.github.com>
2019-11-14 07:38:15 -0700

    Update MemberList.java

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,76 +1,65 @@
-import java.io.Serializable;
-import java.util.Scanner;
-import java.util.LinkedList;
+import java.util.LinkedList;	
+import java.util.Iterator;
+import java.util.*;
+import java.io.*;
+public class MemberList implements Iterable<Member>, Serializable
 
-public class Member implements Serializable
-
-{
-    public String name;
-
-    public int year;
-    
-    public int sum;
-    
-	Scanner kb = new Scanner(System.in);
-    
-	LinkedList<Interest> interestList = new LinkedList<Interest>();
+{	//Create List for members
+	//add save and load method for memberList
 	LinkedList<Member> memberList = new LinkedList<Member>();
-	LinkedList<Member> topFive = new LinkedList<Member>();
 	
-    public Member (String name, int year) 
-
-    {
-
-    	this.name = name;
-    	this.year = year;
-
-    }
-
-   public void changeName(String name) {
-	   this.name = name;
-   }
-   
-   public void changeYear(int year) {
-	   this.year = year;
-   }
-   
-   public void addInterest(String topic, int topicInterest)
-  {
-	for(Interest in : interestList)
-	{
-		if(topic.equalsIgnoreCase(in.getTopic()))
-		{
-			in.changeTopicIntrest(topicInterest);
-		}
+public void addMember(String name, int year) 
+{
+	//Create member object with parameters for name and year
+	//If Member name is unique then add, if not then change name
+	Member M = new Member(name, year);
+		
+	memberList.add(M);
+		
+		
 	}
-   }
-   
-   public String getName()
-   {
-	   return name;
-   }
-   
-   
-   public double calculateCompatability(Member m2)
-   {
-	   for( Interest i : this.interestList) 
-	   {
-		 for (Interest i2 : m2.interestList)
-		 {
-			 if(i.getTopic().equalsIgnoreCase(i2.getTopic())) 
-			 {
-				 sum = sum + (i.getTopicInterest() * i2.getTopicInterest());
-			 }
-			 else
-			 {
-				sum = sum + (i2.getTopicInterest() / 2); 
-			 }
-		 }
-	   }
-	  
-	   
-   }
-   
-   
-   
+	// Instantiate iterator and get its methods
+public Iterator<Member> iterator() 	
+	{
+		return memberList.iterator();
+	}
+	//add method for comparing unique name using for each 
+public Member existingMember(String name)
+{
+	Member found = null;
+	
+	for(Member m: memberList)
+		{
+			if(name.equals(m.getName()))
+			{
+				found = m;
+			}
+		}
+		
+		return found;
+	}
+public void removeMember()
+{
+	memberList.remove();
+}
+	//save file
+public void save(String fileName) throws IOException
+{
+	FileOutputStream fos = new FileOutputStream(fileName); 
+	ObjectOutputStream oos = new ObjectOutputStream(fos); 
+	oos.writeObject(this); 
+	oos.flush(); 
+	oos.close(); 
+}
+	//load members
+public static MemberList load(String fileName) throws IOException, ClassNotFoundException
+{
+	FileInputStream fis = new FileInputStream(fileName);
+	ObjectInputStream ois = new ObjectInputStream(fis);
+	MemberList ml = (MemberList) ois.readObject();
+	ois.close();
+		
+	return ml;
+}
+
 }
