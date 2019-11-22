c26491fb6281d2a2f2ccc5b477d2e9d8b1108477
OSV2130648 <57408616+OSV2130648@users.noreply.github.com>
2019-11-21 22:29:00 -0700

    Update CSCMatch.java

--- src/CSCMatch.java
+++ src/CSCMatch.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class CSCMatch implements Serializable
 {
-   static MemberList membership = new MemberList();
+    static MemberList membership = new MemberList();
     public static void main(String[] args)
     {
         //Introduction to the program
@@ -33,6 +33,8 @@ public class CSCMatch implements Serializable
         //Switch case menu
 
         int select = 0;
+
+        int save = 0;
 
         boolean work = true;
 
@@ -80,20 +82,24 @@ public class CSCMatch implements Serializable
 
                         }
 
-                       // File  userFile = new File(fileCheck);
+                         File  userFile = new File(fileCheck);
 
 
-                      /*  if(!userFile.exists())
+                        if(!userFile.exists())
+
                         {
                             System.out.println("That file was not found, please review file and retry option from menu.");
                         }
+
                         else if (userFile.exists())
+
                         {
                             System.out.println("File was found.");
                         membership = MemberList.load(fileCheck);
-                            System.out.println("Member Loaded.");
-                        }*/
-                        membership = MemberList.load(fileCheck);
+                            System.out.println("Member/s Loaded.");
+                        }
+
+                        //membership = MemberList.load(fileCheck);
                         break;
 
 
@@ -108,18 +114,22 @@ public class CSCMatch implements Serializable
 
                         String fileName = file1.next();
 
-                       // File  loadingFile = new File(fileName);
-                        
-                        membership.save(fileName);
+                         File  loadingFile = new File(fileName);
 
-                     /*  if (!loadingFile.canWrite())
+                        // membership.save(fileName);
+
+                        if (loadingFile.exists())
+
                         {
-                            System.out.println ("File cannot be written to. Please review file or enter other file from menu.");
+
+                            if (!loadingFile.canWrite()) {
+                                System.out.println("File cannot be written to. Please review file or enter other file from menu.");
+                            } else if (loadingFile.canWrite()) {
+                                membership.save(fileName);
+                            }
+
                         }
-                        else if (loadingFile.canWrite())
-                        {
-                       save(fileName);
-                    }*/
+
                         break;
 
 
@@ -144,6 +154,8 @@ public class CSCMatch implements Serializable
                             if (allMembers.getYear() == 5) { Grade = "Graduated";}
 
                             System.out.println(allMembers.getName() + ": " + Grade);
+
+                            System.out.print("");
 
                         }
 
@@ -171,6 +183,11 @@ public class CSCMatch implements Serializable
                                 {
                                     System.out.println(" That Name is already Taken, please enter another.");
                                 }
+                                
+                                else if (name.contains("regress"))
+                                {
+                                    break;
+                                }
 
                             } while (membership.existingMember(name) != null);
 
@@ -178,9 +195,22 @@ public class CSCMatch implements Serializable
 
                             do
 
-                                {
+                            {
 
-                                System.out.println("Enter member year between 1-5");    //Ensure that user input meets parameters
+                                System.out.println("Enter member year between 1-5 to designate year of education.");    //Ensure that user input meets parameters
+
+                                System.out.println(" 1 = Freshman");
+
+                                System.out.println(" 2 = Sophomore");
+
+                                System.out.println(" 3 = Junior");
+
+                                System.out.println(" 4 = Senior");
+
+                                System.out.println(" 5 = Graduated");
+
+                                System.out.println("Enter year:");
+
 
                                 year = kb.nextInt();
 
@@ -188,7 +218,7 @@ public class CSCMatch implements Serializable
 
                                 {
 
-                                    System.out.println("Please enter a year between 1 and 5.");
+                                    System.out.println("Please only enter a value between 1 and 5.");
 
                                 }
 
@@ -197,6 +227,8 @@ public class CSCMatch implements Serializable
                             membership.addMember(name, year);
 
                             System.out.println("Member Successfully Added");
+
+                            save = 1;
                         }
 
 
@@ -228,7 +260,17 @@ public class CSCMatch implements Serializable
 
                             m = membership.existingMember(badName);
 
-                            if (m == null)  // generic test for listing member as well
+                            if (badName.contains("regress"))
+
+                            {
+
+                                System.out.println("Returning to Menu");
+
+                                break;
+
+                            }
+
+                            else if (m == null)  // generic test for listing member as well
 
                             {
                                 System.out.println("That name is not in the database. Please enter existing member name.");
@@ -236,7 +278,16 @@ public class CSCMatch implements Serializable
 
                         } while (m == null);
 
-                        membership.removeMember(m);
+                       // membership.removeMember(m);
+
+                        if (!badName.contains("regress")) {
+
+                            membership.removeMember(m);
+
+                            System.out.println("Member removed.");
+
+                            save = 1;
+                        }
 
                         break;
 
@@ -260,52 +311,54 @@ public class CSCMatch implements Serializable
                             //if (membership.existingMember(idName) != null)  // generic test for listing member as well
 
                             {
-                            	boolean listFound = false;
-                            	for(Member m1 : membership)
-                            	{
-                            		if(m1.getName().equalsIgnoreCase(idName))
-                            		{
-                            			listFound = true;
-		                            	System.out.println("Member found.");
-		                            	
-		                            	System.out.println();
-		                            	
-		                            	System.out.println(m1.getName() + ": ");
-		                            	
-		                            	for(Interest iList : m1.interestList) {
-		                            		System.out.print(iList.getTopic() + ": " + iList.getTopicInterest());	
-		                            		System.out.println();
-		                            	}
-		                            	
-		                            	System.out.println();
-		                            	
-		                                for(Member m2 : membership)
-		                                {
-		                                	m2.score = m1.calculateCompatability(m2);
-		                                	Match newMatch = new Match(m2.getName(),m2.getScore());
-		                                	m1.addMatch(newMatch);
-		                                }
-                            		
-		                      
-		                                if(m1.top5.isEmpty() == false)
-		                                {
-		                                	for( int i = 0; i < 5; i++)
-			                                {
-			                                	System.out.println(m1.top5.get(i).getMatchName());
-			                                	
-			                                }
-		                                }
-		                                else {
-		                                	System.out.println("Matches list is empty");
-		                                }
-                            		}                            
-                               
-                            	}
-                            if (listFound == true)
-                            {
-                            	System.out.println("No member found");
-                            	return;
-                            }
+                            	
+                                for(Member m1 : membership)
+                                {
+                                    if (idName.contains("regress"))
+
+                                    {break;}
+
+                                    else if(m1.getName().equalsIgnoreCase(idName))
+                                    {
+                                    	
+                                        System.out.println("Member found.");
+                                        
+                                        System.out.println();
+
+                                        System.out.println(m1.getName() + ": ");
+                                        
+                                 	
+                                        for(Interest iList : m1.interestList) {
+                                        	
+                                            System.out.print(iList.getTopic() + ": " + iList.getTopicInterest());
+                                            System.out.println();
+                                        	
+                                        }
+
+                                        System.out.println();
+
+                                        /* for(Member m2 : membership)
+                                        {
+                                            m2.score = m1.calculateCompatability(m2);
+                                            Match newMatch = new Match(m2.getName(),m2.getScore());
+                                            m1.addMatch(newMatch);
+                                            break;
+                                        }
+                                        
+
+                                        if(m1.top5.isEmpty() == false)
+                                        {
+                                            for( int i = 1; i < 5; i++)
+                                            {
+                                                if(m1.top5.size() > i) {
+                                            	System.out.println(m1.top5.get(i).getMatchName());
+                                                }
+
+                                            }
+                                        } */
+                                    }
+
+                                }
                             }
 
                         }
@@ -315,7 +368,7 @@ public class CSCMatch implements Serializable
                         break;
 
 
-                  case 7:
+                    case 7:
 
                         System.out.println("Add Interest to Member");
 
@@ -331,65 +384,99 @@ public class CSCMatch implements Serializable
 
                             boolean found = false;
                             for(Member findMem : membership)
-                            if(findMem.getName().equalsIgnoreCase(newName))	
-                            { 
-                            	found = true;
-                            	try
-                            	{
-                            	System.out.println("Enter name of interest");
-                            	String newInterest = kb4.nextLine();
-                            	
-                            	  System.out.println("Enter the level of interest. 0 - 10");
-                            	 boolean valid = false;
-                                 while(!valid)
-                                 { 
-                              	int interestLevel = kb4.nextInt();
-                              	   
-                              	   if(interestLevel >= 0 && interestLevel <=10)
-                              	   {
-                              	   findMem.addInterest(newInterest, interestLevel);
-	                                   System.out.println("Interest added! ");    
-	                                   valid = true;
-                              	   }
-                              	   else
-                              	   {
-                              		   System.out.println("Not valid, enter input again");
-                              		
-                              	   }
-                                    
-                                 }  
-                            	}
-                            	catch (InputMismatchException e) {
-                            		
-                            	}
-                            } kb4.nextLine();
-                          if(found == false)
-                          {
-                        	  System.out.println("Member was not found.");
-                          }
-                          }	
-                        
-                        while (membership.existingMember(newName) != null);
-                        
-                            	
-                            	break;
-                
 
+                                if (newName.contains("regress"))
+                                {
+                                    System.out.println("Returning to Menu. Please press enter.");
+
+                                    break;
+                                }
+
+                                else if(findMem.getName().equals(newName))
+                                {
+                                    found = true;
+                                    try
+                                    {
+                                        System.out.println("Enter name of interest");
+                                        String newInterest = kb4.nextLine();
+
+                                        System.out.println("Enter the level of interest. 0 - 10");
+                                        boolean valid = false;
+                                        while(!valid)
+                                        {
+                                            int interestLevel = kb4.nextInt();
+
+                                            if(interestLevel >= 0 && interestLevel <=10)
+                                            {
+                                                findMem.addInterest(newInterest, interestLevel);
+                                                System.out.println("Interest added! ");
+                                                valid = true;
+                                                save= 1;
+                                            }
+                                            else
+                                            {
+                                                System.out.println("Not valid, enter input again");
+
+                                            }
+
+                                        }
+                                    }
+                                    catch (InputMismatchException e) {
+
+                                    }
+                                } kb4.nextLine();
+
+                            if(found == false)
+                            {
+                                System.out.println("Member was not found.");
+                            }
+                        }
+
+                        while (membership.existingMember(newName) != null);
+
+
+                        break;
 
                     case 8:
 
-                        //if () {}
+                        Scanner kb6 = new Scanner(System.in);
 
-                        //else if ()
+
+                        if (save == 1)
 
                         {
-                        
-                            System.out.println("Thank you for using CSC Match.");
 
-                            System.exit(0);
-                            System.out.println();
+                            System.out.println("Warning, changes have not been saved, are you sure you want to continue? [y,n]");
+
+                            String saveCheck = kb6.nextLine();
+
+                            if (saveCheck.contains("y"))
+
+                            {
+
+                                System.out.println("Thank you for using CSC Match.");
+
+                                System.exit(0);
+
+                                System.out.println();
+
+                            }
+
+                            else if (saveCheck.contains("n")) {break;}
 
                         }
+
+                        else if ( save == 0)
+
+                    {
+
+                        System.out.println("Thank you for using CSC Match.");
+
+                        System.exit(0);
+
+                        System.out.println();
+
+                    }
                 }
 
 
@@ -406,16 +493,18 @@ public class CSCMatch implements Serializable
             {
                 System.out.println("Please enter a number 1-8");
             } catch (IOException e) {
+            	
+            	System.out.println("Please enter a number 1-8");
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
+            	System.out.println("Please enter a number 1-8");
                 e.printStackTrace();
             }
 
         work = false;
-    
-    }
-    
 
-    
     }
 
+
+
+}
